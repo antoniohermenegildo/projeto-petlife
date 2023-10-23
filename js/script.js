a3b9d9856b7a94cd7f136139f184aa89cb5db5f3
antoniohermenegildo <antoniohermen@gmail.com>
2023-10-23 12:29:41 -0300

    projeto PetLife

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var elementoDuvidas = document.querySelectorAll('.duvida');
+elementoDuvidas.forEach(function (duvida){
+  duvida.addEventListener('click', function(){
+    duvida.classList.toggle('ativa')
+  })
+})
