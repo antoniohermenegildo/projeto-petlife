6a7172ab4e8e246fbed2cd8abf7b4d2025fc9d57
antoniohermenegildo <antoniohermen@gmail.com>
2023-11-01 00:41:08 -0300

    ajuste de estilo para responsividade

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+//________ Configuração do menu hamburguer ______________
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburguer.addEventListener("click", () =>{
+  hamburguer.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+//________ Configuração dos itens de duvidas ______________
 var elementoDuvidas = document.querySelectorAll('.duvida');
 elementoDuvidas.forEach(function (duvida){
   duvida.addEventListener('click', function(){
